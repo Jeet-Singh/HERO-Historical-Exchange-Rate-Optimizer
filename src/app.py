969f079b829e0d11b767bf0b7d1fb2687d74cbdd
Jeet-Singh <jeet2748@gmail.com>
2024-08-20 17:48:15 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -186,5 +186,5 @@ if __name__ == '__main__':
 
     if count == 0:
         fetch_and_store_historical_data()
-    
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
